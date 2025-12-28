b52b564462bd2be5530ae87ac1b49f10aae1e3c2
Khushbu Mistry <70948481+khushbu-mistry-nz@users.noreply.github.com>
2025-12-28 15:31:39 +1300

    Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, title: 'Home' },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberList, title: 'Members' },
+            { path: 'members/:id', component: MemberDetailed, title: 'Member Details' },
+            { path: 'lists', component: Lists, title: 'Lists' },
+            { path: 'messages', component: Messages, title: 'Messages' },
+        ]
+    },
+
+    { path: '**', component: Home, title: 'Home' }
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,42 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
-import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private http = inject(HttpClient);
-  private accountService = inject(AccountService);
-  protected readonly title = 'Dating App';
-  protected members= signal<any>([]);
-
- async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user: User = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/Members'));
-    } catch (error) {
-      console.error('An error occurred while fetching members:', error);
-      throw error;
-    }
-  }
+export class App {
+  protected router = inject(Router);
+  
 }
