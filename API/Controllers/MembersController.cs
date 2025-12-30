0793fbf2d48ee63634c3ca473b4cccb42067397e
Khushbu Mistry <70948481+khushbu-mistry-nz@users.noreply.github.com>
2025-12-30 14:27:51 +1300

    update member

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,5 +1,7 @@
 using API.Controllers;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +34,34 @@ namespace DatingApp.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+
+            if (member == null) return BadRequest("Could not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+
+            memberRepository.Update(member);
+
+            if (await memberRepository.SaveAllAsync())
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest("Failed to update member");
+            }
+
         }
     }
 }
